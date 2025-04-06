4c209cf6efb570d152a0e6b54959b0b7fa2d3648
Harish@1530 <codingharshal@gmail.com>
2025-04-06 22:46:56 +0530

    Practiced Looping

--- QT/qt6p1/main.cpp
+++ QT/qt6p1/main.cpp
@@ -17,5 +17,15 @@ int main(int argc, char *argv[])
     // Output the user's name and age
     qInfo() << name.c_str() << "is" << age << "years old.";
 
+    // Control Statements
+
+    if (age < 18) {
+        qInfo() << "You are a minor.";
+    } else if (age >= 18 && age < 65) {
+        qInfo() << "You are an adult.";
+    } else {
+        qInfo() << "Damn! Your are old!";
+    }
+
     return a.exec();
 }
